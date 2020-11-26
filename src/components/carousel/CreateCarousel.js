2986958081ff6878dddf0b94e7f12b7810165fcf
AlaaMostapha <alaamoustapha21@gmail.com>
2020-11-26 11:59:24 +0200

    comment

--- src/components/carousel/CreateCarousel.js
+++ src/components/carousel/CreateCarousel.js
@@ -24,11 +24,11 @@ import './Carousel.scss';
     ]
     const breakPoints=[
         { width: 1, itemsToShow: 1 },
-        { width: 550, itemsToShow: 2, itemsToScroll: 2, pagination: false },
+        { width: 550, itemsToShow: 2, itemsToScroll: 2, },
         { width: 850, itemsToShow: 3 },
         { width: 1150, itemsToShow: 4, itemsToScroll: 2 },
-        { width: 1450, itemsToShow: 5 },
-        { width: 1750, itemsToShow: 6 },
+        // { width: 1450, itemsToShow: 5 },
+        // { width: 1750, itemsToShow: 6 },
     ]
 function Item(props)
 {
